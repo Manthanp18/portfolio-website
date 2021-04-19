e47db1a638b734172624a3979853a76f1cfd85ff
Manthan Patel <patelmanthan1806@gmail.com>
2021-04-19 09:00:02 +0530

    first commit

---
+++ src/components/contactus/contactus.js
@@ -0,0 +1,18 @@
+// import React, { Component } from 'react'
+// export default class ContactUs extends Component {
+//   render () {
+//     let resumeData = this.props.resumeData
+//     return (
+//       <section id='contact'>
+//         <div className='row'>
+//           <aside className='eigth columns footer-widgets'>
+//             <div className='widget'>
+//               <h4>Email :  {resumeData.email}</h4>
+//               <h4>LinkedIn :  @{resumeData.linkedinId}</h4>
+//             </div>
+//           </aside>
+//         </div>
+//       </section>
+//     )
+//   }
+// }
---
+++ src/resumeData.js
@@ -0,0 +1,100 @@
+let resumeData = {
+  imagebaseurl: 'https://techonoeticsclub.github.io/',
+  name: 'Manthan Patel',
+  role: 'MERN Stack Developer',
+  linkedinId: 'manthan1806',
+  skypeid: 'Your skypeid',
+  roleDescription:
+    'I like to learn about new technologies, write technical tweets on twitter or simply play games in my free time.',
+  socialLinks: [
+    {
+      name: 'linkedin',
+      url: 'https://www.linkedin.com/in/manthan1806/',
+      className: 'fa fa-linkedin'
+    },
+    {
+      name: 'github',
+      url: 'https://github.com/Manthanp18',
+      className: 'fa fa-github'
+    },
+    {
+      name: 'twitter',
+      url: 'https://twitter.com/_manthann18',
+      className: 'fa fa-twitter'
+    }
+  ],
+  aboutme:
+    'I am a self-taught developer. I completed my bachelors in commerce. In this COVID lockdown, I started learning web development and got my hands on it. There was always a spark in me to play with computers and tech overall, which led me to start learning to build web applications. I mostly used Udemy to learn and enhance my skills. In this past year or so I learned MERN stack. To practice my learnings I tried building a couple of tiny projects which has been pushed on Github. I am a quick learner and a tech enthusiast who loves to solve problems with technology. To start off my career I am looking for a full-time job where I can learn, grow and contribute to companies growth. ',
+  address: 'Ahmedabad, Gujarat',
+  email: 'patelmanthan1806@gmail.com',
+  education: [
+    {
+      UniversityName: 'Gujarat University',
+      specialization: 'B.COM',
+      MonthOfPassing: 'April',
+      YearOfPassing: '2020'
+    },
+    {
+      UniversityName: 'Udemy',
+      specialization:
+        'React- Complete guide by Acedemind, NodeJS - Building RESTfull Api',
+      MonthOfPassing: 'March',
+      YearOfPassing: '2020',
+      Achievements: ''
+    }
+  ],
+  // work: [
+  //   {
+  //     CompanyName: 'Some Company',
+  //     specialization: 'Some specialization',
+  //     MonthOfLeaving: 'Jan',
+  //     YearOfLeaving: '2018',
+  //     Achievements: 'Some Achievements'
+  //   },
+  //   {
+  //     CompanyName: 'Some Company',
+  //     specialization: 'Some specialization',
+  //     MonthOfLeaving: 'Jan',
+  //     YearOfLeaving: '2018',
+  //     Achievements: 'Some Achievements'
+  //   }
+  // ],
+  // skillsDescription: 'Your skills here',
+  skills: [
+    {
+      skillname: 'HTML5'
+    },
+    {
+      skillname: 'CSS'
+    },
+    {
+      skillname: 'Reactjs'
+    },
+    {
+      skillname: 'Nodejs'
+    },
+    {
+      skillname: 'MongoDB'
+    }
+  ],
+  portfolio: [
+    {
+      name: 'Linkr',
+      description: 'A Linktree Clone with MERN Stack',
+      imgurl: 'images/portfolio/linktree.png'
+    },
+    {
+      name: 'Memories App',
+      description: 'This App Store our memories',
+      imgurl: 'images/portfolio/memories.jpg'
+    },
+    {
+      name: 'BlogMe',
+      description: 'BlogMe is blogging Platform',
+      imgurl: 'images/portfolio/blogg.jpg'
+    }
+  ],
+ 
+}
+
+export default resumeData
